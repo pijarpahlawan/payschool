bb07432cd2c3b1d4d0465cfa77d054eff32c9aa4
Pijar Pahlawan <95151798+pijarpahlawan@users.noreply.github.com>
2024-11-21 03:04:57 +0700

    create role column and user admin seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'role' => 'admin',
+            'password' => bcrypt('admin'),
         ]);
     }
 }
